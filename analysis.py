2853b128b418543820bcfc0d54bab7a6d1b43440
Mumtaaz <64772468+MumtaazM@users.noreply.github.com>
2024-04-08 15:25:03 -0400

    std scaler import

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.discriminant_analysis import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
